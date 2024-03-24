c240329fe135a32fe392bf303493364affa9ef5d
DIONG666 <8208220511@csu.edu.cn>
2024-03-25 00:41:01 +0800

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,12 +4,12 @@ const {createProxyMiddleware: proxy} = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         proxy('/accountLogin', {
-            target: 'http://192.210.174.146/users/login-with-account', //配置转发目标地址(能返回数据的服务器地址)
+            target: 'http://100.65.12.221/users/login-with-account', //配置转发目标地址(能返回数据的服务器地址)
             changeOrigin: true, //控制服务器接收到的请求头中host字段的值
             pathRewrite: {'^/accountLogin': ''} //去除请求前缀，保证交给后台服务器的是正常请求地址(必须配置)
         }),
         proxy('/accountRegister', {
-            target: 'http://192.210.174.146//users/register-with-account', //配置转发目标地址(能返回数据的服务器地址)
+            target: 'http://100.65.12.221/users/register-with-account', //配置转发目标地址(能返回数据的服务器地址)
             changeOrigin: true, //控制服务器接收到的请求头中host字段的值
             pathRewrite: {'^/accountRegister': ''} //去除请求前缀，保证交给后台服务器的是正常请求地址(必须配置)
         }),
