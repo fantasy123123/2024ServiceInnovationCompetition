b891dd76acef1fa2ddc8560b75af94e6a1f4a725
xdk <1455045838@qq.com>
2024-04-08 00:57:14 +0800

    fix

--- src/Pages/SignInPage/component/AccountSignIn.js
+++ src/Pages/SignInPage/component/AccountSignIn.js
@@ -5,14 +5,14 @@ import {IconSafe, IconUnlock, IconUser} from '@arco-design/web-react/icon';
 import {useEffect, useRef, useState} from 'react';
 import { useNavigate} from "react-router-dom";
 import axios from "axios";
-import PicAuthCode from "./PicAuthCode";
 
 const FormItem = Form.Item;
 
 const AccountSignIn = () => {
-    const [name,setName]=useState('')
-    const [password,setPassword]=useState('')
+    let name=''
+    let password=''
     let inputCode=''
+    let code=''
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const navigate=useNavigate()
@@ -27,23 +27,117 @@ const AccountSignIn = () => {
     });
   }, []);
 
-    const setTempCode=()=>{
-        const words='AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz123456789'
-        let tempCode=''
-        for(let i=0;i<4;i++){
-            tempCode+=words[Math.floor( Math.random()*52)]
-        }
-        console.log(tempCode)
-        return tempCode
-    }
+    function PicAuthCode() {
+        const [config]=useState({
+            contentWidth: 100,
+            contentHeight: 35,
+            backgroundColorMin: 180,
+            backgroundColorMax:240,
+            fontSizeMin: 25,
+            fontSizeMax: 30,
+            colorMin: 50,
+            colorMax: 160,
+            lineColorMin:40,
+            lineColorMax: 180,
+            dotColorMin: 0,
+            dotColorMax: 255,
+            textStyle:{fontSize:'12px',color:'gray',marginLeft:'6px',cursor:'pointer',padding:'5px',userSelect:"none"}
+        })
+        const [identifyCode,setIdentifyCode]=useState('')
 
+        const canvasRef=useRef()
+
+        useEffect(()=>{
+            refresh()
+        },[])
+
+        useEffect(()=>{
+            identifyCode && drawPic()
+        })
+
+        PicAuthCode.defaultProps={
+            setCode:()=>""      // 更新验证码的函数
+        }
+
+        const drawPic=()=>{
+            let canvas = canvasRef.current
+            let ctx = canvas.getContext('2d')
+            ctx.fillStyle = randomColor(config.backgroundColorMin,config.backgroundColorMax)
+            ctx.strokeStyle = randomColor(config.backgroundColorMin, config.backgroundColorMax)
+            ctx.fillRect(0, 0, config.contentWidth,config.contentHeight)
+            ctx.strokeRect(0,0,config.contentWidth,config.contentHeight)
+            for (let i = 0; i < identifyCode.length; i++) {
+                drawText(ctx,identifyCode[i], i)
+            }
+            drawLine(ctx)
+            drawDot(ctx)
+        }
+        const randomNum=(min, max)=>{
+            return Math.floor(Math.random() * (max - min) + min)
+        }
+        const randomColor=(min, max)=>{
+            let r = randomNum(min, max)
+            let g = randomNum(min, max)
+            let b = randomNum(min, max)
+            return 'rgb(' + r + ',' + g + ',' + b + ')'
+        }
+        const drawText=(ctx, txt, i)=>{
+            ctx.fillStyle = randomColor(config.colorMin,config.colorMax)
+            ctx.font = randomNum(config.fontSizeMin,config.fontSizeMax) + 'px SimHei'
+            ctx.textBaseline = 'alphabetic'
+            let x = (i + 1) * (config.contentWidth / (identifyCode.length + 1))
+            let y = randomNum(config.fontSizeMax, config.contentHeight - 12)
+            let deg = randomNum(-45, 45)
+            ctx.translate(x, y)
+            ctx.rotate(deg * Math.PI / 180)
+            ctx.fillText(txt, 0, 0)
+            ctx.rotate(-deg * Math.PI / 180)
+            ctx.translate(-x, -y)
+        }
+        const drawLine=(ctx)=>{
+            for (let i = 0; i < 8; i++) {
+                ctx.strokeStyle = randomColor(config.lineColorMin,config.lineColorMax)
+                ctx.beginPath()
+                ctx.moveTo(randomNum(0, config.contentWidth), randomNum(0, config.contentHeight)) //设置起点x,y
+                ctx.lineTo(randomNum(0,config.contentWidth), randomNum(0, config.contentHeight)) //绘制直线 x,y 一条当前位置到x,y点的直线
+                ctx.stroke()
+            }
+        }
+        const drawDot=(ctx)=>{
+            for (let i = 0; i < 100; i++) {
+                ctx.fillStyle = randomColor(0, 255)
+                ctx.beginPath()
+                ctx.arc(randomNum(0, config.contentWidth), randomNum(0, config.contentHeight), 1, 0, 2 * Math.PI)
+                ctx.fill()
+            }
+        }
+        const refresh=()=>{
+            const words='AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz123456789'
+            let tempCode=''
+            for(let i=0;i<4;i++){
+                tempCode+=words[Math.floor( Math.random()*52)]
+            }
+            code=tempCode
+            setIdentifyCode(code)
+            console.log(code)
+        }
+
+        return (
+            <canvas
+                ref={canvasRef}
+                width={config.contentWidth}
+                height={config.contentHeight}
+                onClick={refresh}
+            />
+        )
+    }
 
     return (
     <>
       <Form autoComplete="off" ref={formRef}>
         <FormItem field="用户名/邮箱" rules={[{ required: true }]}>
           <Input
-              onChange={value=>{setName(value)}}
+              onChange={value=>{name=value}}
             placeholder="请输入用户名/邮箱"
             prefix={<IconUser />}
             style={{
@@ -55,7 +149,7 @@ const AccountSignIn = () => {
 
         <FormItem field="密码" rules={[{ required: true }]}>
           <Input
-              onChange={value=>{setPassword(value)}}
+              onChange={value=>{password=value}}
             placeholder="请输入密码"
             prefix={<IconUnlock />}
             style={{
@@ -70,7 +164,7 @@ const AccountSignIn = () => {
               onChange={value=>{inputCode=value}}
               placeholder="请输入验证码"
               prefix={<IconSafe />}
-              suffix={<PicAuthCode setCode={setTempCode} />}
+              suffix={<PicAuthCode/>}
               style={{
                 height: '50px',
                 width: '125%',
@@ -88,8 +182,7 @@ const AccountSignIn = () => {
               borderRadius: '5px',
             }}
             onClick={async () => {
-                console.log(inputCode,setTempCode())
-              if(inputCode===setTempCode()) {
+              if(inputCode===code) {
                   if (formRef.current) {
                       try {
                           await formRef.current.validate();
